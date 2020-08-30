cbcf13c794ebe9bb0059330ec13494c6a1de5e83
Mohammad Kaab <mohammad.kaab@gmail.com>
2020-08-30 13:25:07 +0300

    Init the project

---
+++ tests/Unit/HelloWorldTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class HelloWorldTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testTrueIsEqualTrue()
+    {
+        $this->assertEquals(true, true);
+    }
+
+    public function testTrueIsNotEqualFalse()
+    {
+        $this->assertNotEquals(true, false);
+    }
+}
